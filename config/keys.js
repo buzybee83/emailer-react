97c136768b483a99312de61032662e35a4741f1d
Guivia Moreria <guivia.moreira@platform28.com>
2019-08-23 13:31:05 -0400

    Finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+
+// Check Environment
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
--- services/passport.js
+++ services/passport.js
@@ -1,18 +1,44 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+    .then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
         {
             clientID: keys.googleClientId,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: 'http://localhost:5000/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('accessToken ', accessToken);
-            console.log('refreshToken ',refreshToken);
-            console.log('profile ', profile);
+            User.findOne({googleId: profile.id})
+                .then((existingUser) => {
+                    if (existingUser) {
+                        done(null, existingUser);
+                    } else {
+                        new User({ 
+                            googleId: profile.id,
+                            name: profile.displayName,
+                            email: profile.emails[0].value
+                        })
+                        .save()
+                        .then(user =>  done(null, user));
+                    }
+                }
+            );
         }
     )
 );
