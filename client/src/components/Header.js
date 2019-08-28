8d614c6926eb16a58afd4c7eb6aad78bf0a0f42a
Guivia Moreria <guivia.moreira@platform28.com>
2019-08-28 15:06:37 -0400

    added payment handling

---
+++ client/src/components/Header.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Stripe from './StripeWrapper';
+import { userInfo } from 'os';
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return [
+                    <li key="1"><Stripe /></li>,
+                    <li key="2"> Credits: {this.props.auth.credits} </li>,
+                    <li key="3"><a href="/api/logout">Logout</a></li>
+                ];
+        }
+    }
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper container">
+                    <Link 
+                        to={this.props.auth? '/surveys' : '/'} 
+                        className="left brand-logo">
+                        Emailer
+                    </Link>
+                    <ul id="nav-mobile" className="right">
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- services/passport.js
+++ services/passport.js
@@ -24,22 +24,18 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({googleId: profile.id})
-                .then((existingUser) => {
-                    if (existingUser) {
-                        done(null, existingUser);
-                    } else {
-                        new User({ 
-                            googleId: profile.id,
-                            name: profile.displayName,
-                            email: profile.emails[0].value
-                        })
-                        .save()
-                        .then(user =>  done(null, user));
-                    }
-                }
-            );
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({googleId: profile.id})
+            if (existingUser) {
+                return done(null, existingUser);
+            } 
+
+            const user =  await new User({ 
+                googleId: profile.id,
+                name: profile.displayName,
+                email: profile.emails[0].value
+            }).save();
+            done(null, user);
         }
     )
 );
